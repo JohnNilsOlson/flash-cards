b9904a13de46ff3daebcd085eee0e2119dff3d1a
Michael Watts <Wattsjmichael@gmail.com>
2020-06-02 15:56:51 -0700

    Adds scripts for each indvidual h3
    Co-Authored by JohnNils Olson johnnils@gmail.com

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+  $("#js").click(function() {
+    $("#jsdef").toggleClass();
+  });
+  $("#op").click(function() {
+    $("#opdef").toggleClass();
+  });
+  $("#var").click(function() {
+    $("#vardef").toggleClass();
+  });
+  $("#var-nam-conv").click(function() {
+    $("#var-nam-convdef").toggleClass();
+  });
+  $("#fun").click(function() {
+    $("#fundef").toggleClass();
+  });
+  $("#met").click(function() {
+    $("#metdef").toggleClass();
+  });
+  $("#arg").click(function() {
+    $("#argdef").toggleClass();
+  });
+  $("#par").click(function() {
+    $("#pardef").toggleClass();
+  });
+  $("#ret").click(function() {
+    $("#retdef").toggleClass();
+  });
+  $("#tha").click(function() {
+    $("#thadef").toggleClass();
+  });
+  $("#str").click(function() {
+    $("#strdef").toggleClass();
+  });
+  $("#boo").click(function() {
+    $("#boodef").toggleClass();
+  });
+  $("#und").click(function() {
+    $("#unddef").toggleClass();
+  });
+  $("#nan").click(function() {
+    $("#nandef").toggleClass();
+  });
+  $("#nul").click(function() {
+    $("#nuldef").toggleClass();
+  });
+  $("#ale").click(function() {
+    $("#aledef").toggleClass();
+  });
+  $("#com").click(function() {
+    $("#comdef").toggleClass();
+  });
+  $("#jqu").click(function() {
+    $("#jqudef").toggleClass();
+  });
+  $("#att").click(function() {
+    $("#attdef").toggleClass();
+  });
+});
